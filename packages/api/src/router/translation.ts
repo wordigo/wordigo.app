f67afd0ec271e023edb7fa5cc1b637258621d005
sefa kapısız <sefa.kapisiz@coino.com>
2023-07-10 21:41:33 +0300

    feat: getUserDictionaries, addDictionary added

--- packages/api/src/router/translation.ts
+++ packages/api/src/router/translation.ts
@@ -1,6 +1,7 @@
 import { Translate } from "@google-cloud/translate/build/src/v2"
+import { prisma } from '@wordigo/db'
 import { z } from "zod"
-import { createTRPCRouter, publicProcedure } from "../trpc"
+import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc"
 
 const translate = new Translate({ projectId: process.env.CLOUD_TRANSLATE_PROJECT_ID, key: process.env.CLOUD_TRANSLATE_API_KEY })
 
@@ -12,12 +13,12 @@ export const translationRouter = createTRPCRouter({
       sourceLanguage: z.string(),
       targetLanguage: z.string()
     }))
-    .mutation(async ({ input }) => {
+    .mutation(async ({ ctx, input }) => {
 
       const { sourceLanguage, targetLanguage, query } = input
       const [response, { data }] = await translate.translate(query, { from: sourceLanguage, to: targetLanguage })
       const translatedText = data.translations[0].translatedText
-
+      console.log(ctx.user)
       return {
         success: true,
         translatedText,
@@ -25,4 +26,54 @@ export const translationRouter = createTRPCRouter({
         targetLanguage
       }
     }),
+
+  getUserDictionaries: protectedProcedure
+    .query(async ({ ctx }) => {
+      const { id } = ctx.user
+      const dictionaries = await prisma.dictionaries.findMany({ where: { authorId: id } })
+
+      return {
+        success: true,
+        dictionaries
+      }
+    }),
+
+  // getDictionaryWords: protectedProcedure
+  //   .input(z.object({
+  //     dictionaryId: z.string()
+  //   }))
+  //   .query(async ({ input, ctx }) => {
+  //     const { id } = ctx.user
+  //     const dictionary = await prisma.dictionaries.findFirst({
+  //       where: { authorId: id, id: input.dictionaryId },
+  //       include: { UserWords: true }
+  //     })
+  //     if (!dictionary)
+  //       return {
+  //         success: false,
+  //         dictionary: null
+  //       }
+
+  //     return {
+  //       success: true,
+  //       dictionary
+  //     }
+  //   }),
+
+  addDictionary: protectedProcedure
+    .input(z.object({
+      title: z.string()
+    }))
+    .mutation(async ({ ctx, input }) => {
+
+      const dictionary = {
+        title: input.title,
+        published: true,
+        authorId: ctx.user.id
+      }
+
+      await prisma.dictionaries.create({
+        data: dictionary
+      })
+    })
 })
