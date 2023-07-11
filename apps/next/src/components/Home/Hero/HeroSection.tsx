6e8a8bcb62c79f70248befd9f7694e3576bdc05a
Yusuf Güneş <gunesyusuf838@gmail.com>
2023-07-11 13:09:14 +0300

    Update Landing Hero Animation

--- apps/next/src/components/Home/Hero/HeroSection.tsx
+++ apps/next/src/components/Home/Hero/HeroSection.tsx
@@ -1,13 +1,50 @@
+import { motion } from "framer-motion";
 import { FaCloudDownloadAlt, FaLanguage } from "react-icons/fa";
 import { FaPeopleGroup } from "react-icons/fa6";
 
 import ArrowIcon from "./ArrowIcon";
 
 export default function HeroSection() {
+  const container = {
+    hidden: { opacity: 1, scale: 0 },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      transition: {
+        delayChildren: 0.3,
+        staggerChildren: 0.2,
+      },
+    },
+  };
+
+  const item = {
+    hidden: { y: 20, opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 1,
+    },
+  };
+
   return (
     <section className="ml-[30px] mt-5 w-full">
-      <div className="mt-20 text-[#333549] dark:text-white">
-        <p className="font-semibold text-[14px] text-primary-blue">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+      <motion.div
+        initial={{ x: -1000 }}
+        animate={{ x: 0 }}
+        transition={{
+          duration: 0.5,
+        }}
+        className="mt-20 text-[#333549] dark:text-white"
+      >
+        <motion.p
+          initial={{ x: 2000 }}
+          animate={{ x: 0 }}
+          transition={{
+            duration: 0.5,
+          }}
+          className="font-semibold text-[14px] text-primary-blue"
+        >
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+        </motion.p>
 
         <div className="my-5 text-5xl font-bold">
           <h1>Lorem Ipsum is simply</h1>
@@ -16,51 +53,63 @@ export default function HeroSection() {
           </h1>
         </div>
 
-        <div className="font-semibold text-base opacity-70">
+        <motion.div
+          initial={{ x: 2000 }}
+          animate={{ x: 0 }}
+          transition={{
+            duration: 0.5,
+          }}
+          className="font-semibold text-base opacity-70"
+        >
           <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
           <p>and typesetting industry.</p>
-        </div>
+        </motion.div>
 
         {/* Data Information */}
-        <div className="mt-10 flex flex-col gap-4">
-          <div>
-            <span className="flex gap-3 items-center">
-              <div className="rounded-lg text-3xl text-center">
-                <FaLanguage></FaLanguage>
-              </div>
-              <span>
-                <h1 className="font-semibold text-sm">+10 Language</h1>
-              </span>
+        <motion.div variants={container} initial="hidden" animate="visible" className="mt-10 flex flex-col gap-4">
+          <motion.span variants={item} className="flex gap-3 items-center">
+            <div className="rounded-lg text-3xl text-center">
+              <FaLanguage></FaLanguage>
+            </div>
+            <span>
+              <h1 className="font-semibold text-sm">+10 Language</h1>
             </span>
-          </div>
+          </motion.span>
 
-          <span className="flex gap-3 items-center">
+          <motion.span variants={item} className="flex gap-3 items-center">
             <div className="rounded-lg text-[30px] text-center">
               <FaPeopleGroup></FaPeopleGroup>
             </div>
             <span>
               <h1 className="font-semibold text-sm">+99 Learner</h1>
             </span>
-          </span>
+          </motion.span>
 
-          <span className="flex gap-3 items-center">
+          <motion.span variants={item} className="flex gap-3 items-center">
             <div className="rounded-lg text-[30px] text-center">
               <FaCloudDownloadAlt></FaCloudDownloadAlt>
             </div>
             <span>
               <h1 className="font-semibold text-sm">+99 Downloads</h1>
             </span>
-          </span>
-        </div>
+          </motion.span>
+        </motion.div>
 
-        <div className="w-full relative bg-black  mt-[-100px]">
+        <motion.div
+          initial={{ rotate: -45 }}
+          animate={{ rotate: 0 }}
+          transition={{
+            duration: 0.7,
+          }}
+          className="w-full relative bg-black  mt-[-100px]"
+        >
           <span className="absolute right-0">
             <ArrowIcon />
           </span>
 
           <div className="right-[170px] mt-[130px] absolute bg-gray-400 py-[10px] px-[25px] rounded-lg w-[320px] h-[120px]"></div>
-        </div>
-      </div>
+        </motion.div>
+      </motion.div>
     </section>
   );
 }
--- apps/next/src/components/Home/Hero/index.tsx
+++ apps/next/src/components/Home/Hero/index.tsx
@@ -1,11 +1,36 @@
+import { motion } from "framer-motion";
+
 import HeroBanner from "./HeroBanner";
 import HeroSection from "./HeroSection";
 
 export default function HeroContainer() {
+  const container = {
+    hidden: { opacity: 1, scale: 0 },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      transition: {
+        delayChildren: 0.3,
+        staggerChildren: 0.2,
+      },
+    },
+  };
+
+  const item = {
+    hidden: { y: -1000, opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 1,
+    },
+  };
+
   return (
-    <main className="m-auto max-w-[1440px] flex justify-between mb-[80px] mt-4">
+    <motion.main variants={container} initial="hidden" animate="visible" className="m-auto max-w-[1440px] flex justify-between mb-[80px] mt-4">
       <HeroSection />
-      <HeroBanner />
-    </main>
+
+      <motion.section className="item" variants={item}>
+        <HeroBanner />
+      </motion.section>
+    </motion.main>
   );
 }
