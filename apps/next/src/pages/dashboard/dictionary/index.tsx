58e486a0168f60addb340f0c734747c7453d0c88
Yusuf Güneş <gunesyusuf838@gmail.com>
2023-07-12 11:43:01 +0300

    Dictionary Page Data Was Taken

--- apps/next/src/pages/dashboard/dictionary/index.tsx
+++ apps/next/src/pages/dashboard/dictionary/index.tsx
@@ -1,20 +1,38 @@
+import { useEffect, useState } from "react";
+import { CreateDictionary } from "@/components/Dashboard/Dictionary/CreateDictionary";
 import { columns } from "@/components/Dashboard/Dictionary/data-table/columns";
 import { DataTable } from "@/components/Dashboard/Dictionary/data-table/data-table";
-import tasks from "@/components/Dashboard/Dictionary/data-table/tasks.json";
 import DashboardLayout from "@/components/Layout/Dashboard";
 import { DashboardHeader } from "@/components/Layout/Dashboard/Header";
 import { DashboardShell } from "@/components/Layout/Dashboard/Shell";
+import { api } from "@/libs/trpc";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
 export default function DashboardPage() {
+  // //const deneme = api.dictionary.addDictionary.useMutation()
+  // //deneme.mutation
+
+  //Fix Type!
+  const [data, setData] = useState<any>();
+
+  const dataQuery = api.dictionary.getUserDictionaries.useQuery();
+
+  useEffect(() => {
+    if (dataQuery.data) {
+      setData(dataQuery.data.data);
+    }
+  }, [dataQuery.data]);
+
   return (
-    <DashboardLayout heading="Dictionary">
-      <DashboardShell>
-        <DashboardHeader>
-          <DataTable columns={columns} data={tasks} />
-        </DashboardHeader>
-      </DashboardShell>
-    </DashboardLayout>
+    <>
+      <DashboardLayout heading="Dictionary">
+        <DashboardShell>
+          <DashboardHeader>
+            <DataTable columns={columns} data={data} />
+          </DashboardHeader>
+        </DashboardShell>
+      </DashboardLayout>
+    </>
   );
 }
 
