d04ba4c925e6533692f9bdec57c03fea628edb5b
sefakpsz <m.sefa06@hotmail.com>
2023-07-16 13:11:05 +0300

    remove index codes

--- apps/next/src/pages/index.tsx
+++ apps/next/src/pages/index.tsx
@@ -1,28 +1,13 @@
-import { useEffect } from "react";
 import FeatureBanner from "@/components/Home/Banner";
 import Comment from "@/components/Home/Comments";
 import FeatureSections from "@/components/Home/FeatureSections";
 import Footers from "@/components/Home/Footers";
 import HeroContainer from "@/components/Home/Hero";
 import MainLayout from "@/components/Layout/MainLayout";
-import { api } from "@/libs/trpc";
 import { i18n } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
 export default function index() {
-  const addingWord = api.userWord.removeUserWord.useMutation();
-
-  useEffect(() => {
-    addingWord.mutate({
-      wordId: "clk59s6vn000h8pbn6e7pf5w5",
-    });
-
-    // addingDic.mutate({
-    //   title:'testDic',
-    //   published:false
-    // })
-  }, []);
-
   return (
     <MainLayout>
       <div>
