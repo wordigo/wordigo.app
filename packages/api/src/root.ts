98288cb623c53aeb48b9614fac04ba3b14952a01
sefa kapısız <m.sefa06@hotmail.com>
2023-07-17 07:39:17 +0300

    feat: subscribeRouter added

--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -1,10 +1,11 @@
-import { authRouter } from "./router/auth";
-import { dictionaryRouter } from "./router/dictionary";
-import { dictionaryWordRouter } from "./router/dictionaryWord";
-import { translationRouter } from "./router/translation";
-import { userWordRouter } from "./router/userWord";
-import { wordRouter } from "./router/word";
-import { createTRPCRouter } from "./trpc";
+import { authRouter } from "./router/auth"
+import { dictionaryRouter } from "./router/dictionary"
+import { dictionaryWordRouter } from "./router/dictionaryWord"
+import { subscribeRouter } from './router/subscribe'
+import { translationRouter } from "./router/translation"
+import { userWordRouter } from "./router/userWord"
+import { wordRouter } from "./router/word"
+import { createTRPCRouter } from "./trpc"
 
 export const appRouter = createTRPCRouter({
   auth: authRouter,
@@ -13,7 +14,8 @@ export const appRouter = createTRPCRouter({
   dictionaryWord: dictionaryWordRouter,
   word: wordRouter,
   userWord: userWordRouter,
-});
+  subscribe: subscribeRouter
+})
 
 // export type definition of API
-export type AppRouter = typeof appRouter;
+export type AppRouter = typeof appRouter
---
+++ packages/api/src/router/subscribe.ts
@@ -0,0 +1,35 @@
+import { z } from "zod"
+
+import { prisma } from "@wordigo/db"
+
+import messages from '../../../common/constants/messages'
+import { successResult } from '../../../common/constants/results'
+import { createTRPCRouter, publicProcedure } from "../trpc"
+
+export const subscribeRouter = createTRPCRouter({
+  subscribe: publicProcedure
+    .input(
+      z.object({
+        email: z.string(),
+      }),
+    )
+    .mutation(async ({ input }) => {
+      const { email } = input
+
+      await prisma.subscribers.create({
+        data: {
+          email
+        }
+      })
+
+      return successResult(null, messages.success)
+    }),
+
+  getSubscribersList: publicProcedure
+    .query(async () => {
+      const subs = await prisma.subscribers.findMany() as []
+
+      return successResult(subs, messages.success)
+
+    }),
+})
