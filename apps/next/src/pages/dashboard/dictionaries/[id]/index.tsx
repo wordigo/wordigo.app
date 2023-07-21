4cf3a96b2731682250b1fd35e532de2bdefe7af6
Yusuf Güneş <gunesyusuf838@gmail.com>
2023-07-22 01:19:56 +0300

    Fix vercel error

--- apps/next/src/pages/dashboard/dictionaries/[id]/index.tsx
+++ apps/next/src/pages/dashboard/dictionaries/[id]/index.tsx
@@ -1,13 +1,10 @@
-import { Fragment, useEffect } from "react";
-import { type GetStaticPaths } from "next";
+import { Fragment } from "react";
 import { useRouter } from "next/router";
 import { columns } from "@/components/Dashboard/Words/columns";
 import { DataTable } from "@/components/Dashboard/Words/data-table";
 import DashboardLayout from "@/components/Layout/Dashboard";
 import { DashboardShell } from "@/components/Layout/Dashboard/Shell";
 import { api } from "@/libs/trpc";
-import useWordStore from "@/stores/Words";
-import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
 export default function DashboardPage() {
   //   const data = api.dictionaryWord.addWordToDic.useMutation();
@@ -16,7 +13,6 @@ export default function DashboardPage() {
   //     wordId: ""
   //   })
 
-  const { wordList, setWordList } = useWordStore((state) => state);
   const router = useRouter();
   const { id } = router.query;
 
@@ -27,7 +23,7 @@ export default function DashboardPage() {
   return (
     <Fragment>
       <DashboardLayout heading={"Dictionary"} query={data?.data?.dictionary?.title}>
-        <DashboardShell>{data && <DataTable columns={columns} data={data?.data?.dictionary} label="Add Words" />}</DashboardShell>
+        {/* <DashboardShell>{data && <DataTable columns={columns} data={data?.data?.dictionary} label="Add Words" />}</DashboardShell> */}
       </DashboardLayout>
     </Fragment>
   );
