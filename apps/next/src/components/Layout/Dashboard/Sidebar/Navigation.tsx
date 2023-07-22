098d6e5029e00ed916bea29ed626c7d711fa636a
Yusuf Güneş <gunesyusuf838@gmail.com>
2023-07-22 14:11:52 +0300

    Fix : Sidebar Dictionary Selection Menu Style and Bug

--- apps/next/src/components/Layout/Dashboard/Sidebar/Navigation.tsx
+++ apps/next/src/components/Layout/Dashboard/Sidebar/Navigation.tsx
@@ -12,11 +12,9 @@ import useSidebarNavigations, { type SidebarChildNav, type SidebarNavItem } from
 const DashboardNav = () => {
   const navigations = useSidebarNavigations();
 
-  const { showSidebarPanel, setSidebarPanel } = useCommonStore((state) => state);
-
   return (
     <AnimatePresence>
-      <nav className="grid items-start gap-2" onClick={() => setSidebarPanel(!showSidebarPanel)}>
+      <nav className="grid items-start gap-2">
         {navigations.map((item, index) => (
           <DashboardNav.Item {...item} key={index} />
         ))}
@@ -30,18 +28,19 @@ DashboardNav.Item = (item: SidebarNavItem, index: number) => {
   const path = usePathname();
 
   const classes = cn(
-    "flex flex-col hover:bg-accent hover:text-accent-foreground rounded-md font-medium group relative",
-    path === item.href ? "bg-accent" : "transparent",
+    "flex flex-col hover:text-accent-foreground rounded-md font-medium group relative",
+    path === item.href ? "" : "transparent",
     item.disabled && "cursor-not-allowed opacity-80",
   );
 
-  const handleSidebar = () => {
-    setSidebarPanel(!showSidebarPanel);
-  };
-
   return (
     <div className={classes}>
-      <Link key={index} href={item.disabled ? "/" : item.href} className="flex items-center w-full" onClick={handleSidebar}>
+      <Link
+        key={index}
+        href={item.disabled ? "/" : item.href}
+        className="flex items-center w-full"
+        onClick={() => setSidebarPanel(!showSidebarPanel)}
+      >
         <span className="flex items-center px-3 py-2 text-sm w-full">
           {item.icon}
           <span className="flex justify-between w-full items-center">{item.title}</span>
@@ -53,6 +52,7 @@ DashboardNav.Item = (item: SidebarNavItem, index: number) => {
 };
 
 DashboardNav.ChildItem = ({ navs, trigger, loading }: SidebarChildNav) => {
+  const { showSidebarPanel, setSidebarPanel } = useCommonStore((state) => state);
   return (
     <Fragment>
       <div tabIndex={50} className="text-sm z-50 font-medium absolute right-0 top-2 mr-2 flex items-center justify-center">
@@ -61,9 +61,14 @@ DashboardNav.ChildItem = ({ navs, trigger, loading }: SidebarChildNav) => {
       {!loading && (
         <div className="flex flex-col">
           {navs?.map((item, index) => (
-            <Link href={"/dashboard/dictionaries/" + item.link} key={index} className="w-full">
-              <span className={cn("flex items-center px-3 py-2 text-sm w-full")}>
-                <span className="flex justify-between w-full items-center">{item.name}</span>
+            <Link
+              href={"/dashboard/dictionaries/" + item.link}
+              key={index}
+              className="w-full"
+              onClick={() => setSidebarPanel(!showSidebarPanel)}
+            >
+              <span className={cn("flex items-center px-3 text-sm w-full")}>
+                <span className="flex justify-between w-full items-center hover:bg-accent px-3 py-2 rounded-sm">{item.name}</span>
               </span>
             </Link>
           ))}
