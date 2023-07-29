952fe24759ed5beada7e65023eedcc38e325f804
Ali Osman <dlsmnosman@gmail.com>
2023-07-29 21:22:56 +0300

    fix: nav profile

--- apps/next/src/components/Layout/NavProfile/index.tsx
+++ apps/next/src/components/Layout/NavProfile/index.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
-import { useAuthStore } from "@/hooks/useAuthStore";
 import { LogOut } from "lucide-react";
+import { signOut, useSession } from "next-auth/react";
 
 import {
   Avatar,
@@ -18,10 +18,10 @@ import {
 } from "@wordigo/ui";
 
 const NavProfile = ({ variant }: { variant?: "dashboard" }) => {
-  const { user, logout } = useAuthStore();
+  const { data } = useSession();
 
-  const splittedText = user?.user_metadata.name?.split(" ") || ([] as string[]);
-  const computedName = splittedText?.[0]?.[0] + splittedText?.[1]?.[0];
+  const splittedText = data?.user?.name?.toUpperCase()?.split(" ");
+  const computedName = (splittedText?.[0]?.[0] || "") + (splittedText?.[1]?.[0] || "");
 
   return (
     <DropdownMenu>
@@ -30,20 +30,20 @@ const NavProfile = ({ variant }: { variant?: "dashboard" }) => {
           <Button variant={"ghost"} className="w-full justify-between relative h-fit focus-visible:ring-0 focus-visible:ring-offset-0">
             <div className="flex items-center">
               <Avatar className="h-10 w-10">
-                <AvatarImage className="h-10 w-10" src={user?.user_metadata.avatar_url} alt={"@" + user?.user_metadata.name} />
+                <AvatarImage className="h-10 w-10" src={data?.user.avatar_url} alt={"@" + data?.user.name} />
                 <AvatarFallback>{computedName}</AvatarFallback>
               </Avatar>
 
               <DropdownMenuLabel>
                 <div className="flex flex-col">
-                  <p className="text-sm font-semibold leading-5 text-start">{user?.user_metadata.full_name}</p>
+                  <p className="text-sm font-semibold leading-5 text-start">{data?.user.name}</p>
                   <p className="text-sm font-normal leading-5 text-muted-foreground">{"@username"}</p>
                 </div>
               </DropdownMenuLabel>
             </div>
 
             <div>
-              <DropdownMenuLabel className="text-gray-400 right-0 hover:!text-red-500" onClick={logout}>
+              <DropdownMenuLabel className="text-gray-400 right-0 hover:!text-red-500" onClick={() => signOut()}>
                 <LogOut size={"20"} />
               </DropdownMenuLabel>
             </div>
@@ -53,7 +53,7 @@ const NavProfile = ({ variant }: { variant?: "dashboard" }) => {
         <DropdownMenuTrigger>
           <Button variant="ghost" className="relative h-10 w-10 rounded-full focus-visible:ring-0 focus-visible:ring-offset-0">
             <Avatar className="h-10 w-10">
-              <AvatarImage src={user?.user_metadata.avatar_url} alt={"@" + user?.user_metadata.name} />
+              <AvatarImage src={data?.user.avatar_url} alt={"@" + data?.user.name} />
               <AvatarFallback>{computedName}</AvatarFallback>
             </Avatar>
           </Button>
@@ -62,8 +62,8 @@ const NavProfile = ({ variant }: { variant?: "dashboard" }) => {
       <DropdownMenuContent className="w-56">
         <DropdownMenuLabel className="font-normal">
           <div className="flex flex-col space-y-1">
-            <p className="text-sm font-medium leading-none">{user?.user_metadata.full_name}</p>
-            <p className="text-xs leading-none text-muted-foreground">{user?.user_metadata.email}</p>
+            <p className="text-sm font-medium leading-none">{data?.user.name}</p>
+            <p className="text-xs leading-none text-muted-foreground">{data?.user.email}</p>
           </div>
         </DropdownMenuLabel>
         <DropdownMenuSeparator />
@@ -75,7 +75,7 @@ const NavProfile = ({ variant }: { variant?: "dashboard" }) => {
           <DropdownMenuItem>Settings</DropdownMenuItem>
         </DropdownMenuGroup>
         <DropdownMenuSeparator />
-        <DropdownMenuItem className="text-red-400 hover:!text-red-500" onClick={logout}>
+        <DropdownMenuItem className="text-red-400 hover:!text-red-500" onClick={() => signOut()}>
           Log out
         </DropdownMenuItem>
       </DropdownMenuContent>
