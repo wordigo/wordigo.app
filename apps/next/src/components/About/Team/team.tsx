1df1489296893988fae86e09bfdeffcad3a69b65
Mustafa Pekkirişci <pekkiriscim@gmail.com>
2023-08-01 13:14:59 +0300

    feat: create team section

--- apps/next/src/components/About/Team/team.tsx
+++ apps/next/src/components/About/Team/team.tsx
@@ -1,64 +1,56 @@
 import Image from "next/image";
 import Link from "next/link";
-import Animation from "@/components/Animation";
 import { Github, Linkedin, Twitter } from "lucide-react";
 import { useTranslation } from "next-i18next";
-
-import { cn } from "@wordigo/ui/lib/utils";
 
 import commentConstants, { type ITeam } from "./team.constants";
 
 const TeamSection = () => {
   const { t } = useTranslation();
+
   return (
-    <Animation>
-      <section className="max-w-screen-8xl w-full m-auto">
-        <div className="flex flex-col items-center justify-center">
-          <h1 className="text-3xl opacity-80 font-bold tracking-tight">{t("about.teams")}</h1>
-          <div className="gap-6 justify-center flex-wrap items-center py-8 mx-auto max-w-screen-2xl flex lg:py-16 lg:px-6">
-            {commentConstants.map((item) => (
-              <TeamSection.Item key={item.title} {...item} />
-            ))}
-          </div>
-        </div>
-      </section>
-    </Animation>
+    <section className="w-full flex flex-col items-center px-20 py-24">
+      <span className="px-32 w-full flex flex-col">
+        <h2 className="text-4xl font-semibold text-center">{t("about.team.heading")}</h2>
+        <p className="text-xl text-muted-foreground mt-5 text-center">{t("about.team.subheading")}</p>
+      </span>
+      <div className="mt-16 w-full grid grid-cols-4 grid-rows-2 gap-x-8 gap-y-16">
+        {commentConstants.map((item) => (
+          <TeamSection.Item key={item.title} {...item} />
+        ))}
+      </div>
+    </section>
   );
 };
 
 TeamSection.Item = ({ github, linkedin, name, src, title, twitter }: ITeam) => {
   return (
-    <Animation>
-      <div className="w-[280px] h-[300px] px-8 py-5 rounded-xl dark:bg-[#141420] bg-[#F3F4FE] border-[#e9eafe]  text-light_text border dark:text-white dark:border-[rgb(33,32,44)] shadow-lg dark:shadow-black">
-        <span className={cn("flex items-center gap-3 flex-col")}>
-          <span className="border-2 border-gray-600 rounded-full">
-            <Image className="rounded-full m-1" alt="" src={src} width={128} height={128}></Image>
-            {!src && <div className="rounded-full m-1 w-[84px] h-[84px] bg-sky-500"></div>}
-          </span>
-          <h1>{name}</h1>
-          <p className="text-center truncate">{title}</p>
-        </span>
-        <div className="mt-2 flex items-center justify-center">
-          <div className="flex gap-1 items-center">
-            {github && (
-              <Link href={github} target="_blank" className="dark:bg-[#020817] p-2 rounded-full hover:opacity-80">
-                <Github className="text-sm w-[15px] h-[15px]" />
-              </Link>
-            )}
-            {twitter && (
-              <Link href={twitter} target="_blank" className="dark:bg-[#020817]  p-2 rounded-full hover:opacity-80">
-                <Twitter className="text-sm w-[15px] h-[15px]" />
-              </Link>
-            )}
-            {linkedin && (
-              <Link href={linkedin} target="_blank" className="dark:bg-[#020817]  p-2 rounded-full hover:opacity-80">
-                <Linkedin className="text-sm w-[15px] h-[15px]" />
-              </Link>
-            )}
-          </div>
+    <div className="flex flex-col items-center justify-between p-6">
+      <div className="flex flex-col items-center">
+        <div className="relative w-24 h-24">
+          <Image className="rounded-full" alt="" src={src} fill></Image>
         </div>
+        <h3 className="text-lg font-semibold mt-5 text-center">{name}</h3>
+        <p className="mt-2 text-base text-muted-foreground text-center">{title}</p>
       </div>
-    </Animation>
+      <div className="mt-4 grid grid-flow-col gap-x-4 text-muted-foreground">
+        {twitter && (
+          <Link href={twitter} target="_blank">
+            <Twitter fill="currentColor" className="w-5 h-5" />
+          </Link>
+        )}
+        {linkedin && (
+          <Link href={linkedin} target="_blank">
+            <Linkedin fill="currentColor" className="w-5 h-5" />
+          </Link>
+        )}
+        {github && (
+          <Link href={github} target="_blank">
+            <Github fill="currentColor" className="w-5 h-5" />
+          </Link>
+        )}
+      </div>
+    </div>
   );
 };
 
