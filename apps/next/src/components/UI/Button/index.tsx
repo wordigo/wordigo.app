5fedcc3181a70c740c5ef3c5661de4bee227693e
Ali Osman <dlsmnosman@gmail.com>
2023-08-19 16:13:28 +0300

    fix: translate,

--- apps/next/src/components/UI/Button/index.tsx
+++ apps/next/src/components/UI/Button/index.tsx
@@ -1,17 +1,23 @@
-import { Fragment, type PropsWithChildren } from "react";
-import { RotateCw } from "lucide-react";
-
 import { Button, type ButtonProps } from "@wordigo/ui";
+import { RotateCw } from "lucide-react";
+import { useTranslation } from "next-i18next";
+import { Fragment, type PropsWithChildren } from "react";
 
 type CButtonProps = ButtonProps & { loading?: boolean };
 
-const CButton = ({ children, loading, ...attr }: PropsWithChildren<CButtonProps>) => {
+const CButton = ({
+  children,
+  loading,
+  ...attr
+}: PropsWithChildren<CButtonProps>) => {
+  const { t } = useTranslation();
+
   return (
     <Button disabled={loading} {...attr}>
       {loading ? (
         <Fragment>
           <RotateCw className="mr-2 h-4 w-4 animate-spin" />
-          Please wait
+          {t("general.loading")}
         </Fragment>
       ) : (
         children
