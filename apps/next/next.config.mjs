ccf9262d36823724ba539f5f933e4f1ad9a344a5
osmandlsmn <dlsmnosman@gmail.com>
2023-11-12 20:46:24 +0300

    fix: feedback popup slow rendering image problem

--- apps/next/next.config.mjs
+++ apps/next/next.config.mjs
@@ -34,6 +34,31 @@ const config = {
     ];
   },
 
+  webpack(config) {
+    // Grab the existing rule that handles SVG imports
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.(".svg")
+    );
+
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/,
+      },
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        resourceQuery: { not: /url/ }, // exclude if *.svg?url
+        use: ["@svgr/webpack"],
+      }
+    );
+
+    fileLoaderRule.exclude = /\.svg$/i;
+
+    return config;
+  },
+
   ...i18n,
 };
 
