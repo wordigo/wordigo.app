263fd151da3a7e31a6e37d8e93b5229e3974655a
osmandlsmn <dlsmnosman@gmail.com>
2023-11-19 12:34:17 +0300

    feat: add dictionary share dropdown

--- apps/next/src/components/Published/SocialShare/index.tsx
+++ apps/next/src/components/Published/SocialShare/index.tsx
@@ -6,12 +6,14 @@ import {
   DropdownMenuTrigger,
 } from "@wordigo/ui";
 import { Share2Icon } from "lucide-react";
+import { useTranslation } from "next-i18next";
 import React, { PropsWithChildren } from "react";
 import { BsLink45Deg, BsLinkedin, BsTwitter } from "react-icons/bs";
 import { LinkedinShareButton, TwitterShareButton } from "react-share";
 import { IDictionary } from "types/global";
 
 const SocialShare = ({ title, slug }: IDictionary) => {
+  const { t } = useTranslation();
   const host =
     typeof window !== "undefined" ? window.location.origin : undefined;
   const url = `${host}/topic/${slug}`;
@@ -32,28 +34,33 @@ const SocialShare = ({ title, slug }: IDictionary) => {
       <DropdownMenuContent>
         <DropdownMenuItem>
           <TwitterShareButton
+            related={["wordigoapp"]}
             className="w-full flex gap-x-2 items-center"
             url={url as string}
             title={title}
+            hashtags={["wordigo", "dictionary"]}
           >
             <BsTwitter />
-            twitter da paylaş
+            {t("share.twitter")}
           </TwitterShareButton>
         </DropdownMenuItem>
         <DropdownMenuItem>
           <LinkedinShareButton
             className="w-full flex gap-x-2 items-center"
             url={url as string}
-            source="DevSözlük"
+            source="Wordigo"
             title={title}
           >
             <BsLinkedin />
-            linkedin da paylaş
+            {t("share.linkedin")}
           </LinkedinShareButton>
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={copyToClipboard}>
+        <DropdownMenuItem
+          className="w-full flex gap-x-2 items-center cursor-pointer"
+          onClick={copyToClipboard}
+        >
           <BsLink45Deg />
-          linki kopyala
+          {t("share.link_copy")}
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
