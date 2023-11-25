9856ae67dd159dc795ceb5ffb23412db0f9b96ba
Emre Erden <emreerden@pm.me>
2023-11-25 19:43:02 +0300

    edit Footer structure

--- apps/next/src/components/Home/Footers/index.tsx
+++ apps/next/src/components/Home/Footers/index.tsx
@@ -17,184 +17,178 @@ export default function Footer() {
   const { t } = useTranslation();
 
   return (
-    <footer
-      className="relative max-w-screen-8xl w-full m-auto"
-      aria-labelledby="footer"
-      id="footer"
-    >
-      <div className="relative mx-auto w-full max-md:px-4 px-8 max-xl:mb-4 mb-8">
-        <div className="xl:justify-between xl:flex dark:border-white/5 border-t lg:pt-24 max-lg:pt-12 border-black/30 pb-8">
-          <div className="flex flex-col justify-between">
-            <div className="items-start flex flex-col">
-              <div className="max-w-[110px]">
-                <DynamicLogo />
-              </div>
-              <p className="text-left text-xs leading-loose md:text-left dark:text-white/50 text-black/50 max-w-md">
-                {t("footer.wordigo_description")}
-              </p>
-              <div className="flex items-start space-x-2 mt-4">
-                <ChangeLanguage />
-                <ThemeMode />
-              </div>
+    <footer className="relative container" aria-labelledby="footer" id="footer">
+      <section className="xl:justify-between xl:flex dark:border-white/5 border-t lg:pt-24 max-lg:pt-12 border-black/30 pb-8">
+        <div className="flex flex-col justify-between">
+          <div className="items-start flex flex-col">
+            <div className="max-w-[110px]">
+              <DynamicLogo />
+            </div>
+            <p className="text-left text-xs leading-loose md:text-left dark:text-white/50 text-black/50 max-w-md">
+              {t("footer.wordigo_description")}
+            </p>
+            <div className="flex items-start space-x-2 mt-4">
+              <ChangeLanguage />
+              <ThemeMode />
             </div>
           </div>
-          <div className="grid grid-cols-2 gap-10 max-xl:mt-10 max-xl:mb-4">
-            <div className="md:grid md:gap-16 md:grid-cols-2">
-              <div>
-                <h3 className="font-semibold opacity-100 hover:opacity-80">
-                  <Link href={"/"} passHref>
-                    {t("navbar.home")}
-                  </Link>
-                </h3>
-                <ul className="mt-4 space-y-2" role="list">
-                  <li>
-                    <p className="text-sm opacity-70 hover:opacity-100 hover:dark:opacity-100">
-                      <Link href="/blogs" passHref>
-                        {t("navbar.blogs")}
-                      </Link>
-                    </p>
-                  </li>
-                  <ul className="mt-4 space-y-2" role="list">
-                    <li>
-                      <p className="text-sm opacity-70 hover:opacity-100 hover:dark:opacity-100">
-                        <Link href="/library" passHref>
-                          {t("navbar.library")}
-                        </Link>
-                        <span className="text-xs font-medium bg-vulcan-800 inline-flex items-center ml-2 px-2 py-0.5 rounded text-white dark:text-white bg-primary dark:bg-primary-foreground">
-                          {t("general.new")}
-                        </span>
-                      </p>
-                    </li>
-                    <li>
-                      <p className="text-sm opacity-70 hover:opacity-100 hover:dark:opacity-100">
-                        <Link href="/post/privacy-policy" passHref>
-                          {t("privacy_policy.title")}
-                        </Link>
-                      </p>
-                    </li>
-                    <li>
-                      <p className="text-sm opacity-70 hover:opacity-100 hover:dark:opacity-100">
-                        <Link href="/about" passHref>
-                          {t("navbar.about")}
-                        </Link>
-                      </p>
-                    </li>
-                    <li>
-                      <p className="text-sm opacity-70 hover:opacity-100 hover:dark:opacity-100">
-                        <Link href="/faq" passHref>
-                          {t("faq.nav")}
-                        </Link>
-                      </p>
-                    </li>{" "}
-                  </ul>
-                </ul>
-              </div>
-              <div>
-                <Link
-                  href={"/dashboard"}
-                  className="font-semibold opacity-100 hover:opacity-80"
-                >
-                  {t("navbar.dashboard")}
+        </div>
+        <div className="grid grid-cols-2 gap-10 max-xl:mt-10 max-xl:mb-4">
+          <div className="md:grid md:gap-16 md:grid-cols-2">
+            <div>
+              <h3 className="font-semibold opacity-100 hover:opacity-80">
+                <Link href={"/"} passHref>
+                  {t("navbar.home")}
                 </Link>
+              </h3>
+              <ul className="mt-4 space-y-2" role="list">
+                <li>
+                  <p className="text-sm opacity-70 hover:opacity-100 hover:dark:opacity-100">
+                    <Link href="/blogs" passHref>
+                      {t("navbar.blogs")}
+                    </Link>
+                  </p>
+                </li>
                 <ul className="mt-4 space-y-2" role="list">
                   <li>
                     <p className="text-sm opacity-70 hover:opacity-100 hover:dark:opacity-100">
-                      <Link href={"/dashboard/dictionaries"} passHref>
-                        {t("navbar.dictionaries")}
+                      <Link href="/library" passHref>
+                        {t("navbar.library")}
                       </Link>
+                      <span className="text-xs font-medium bg-vulcan-800 inline-flex items-center ml-2 px-2 py-0.5 rounded text-white dark:text-white bg-primary dark:bg-primary-foreground">
+                        {t("general.new")}
+                      </span>
                     </p>
                   </li>
-                </ul>
-                <ul className="mt-4 space-y-2" role="list">
                   <li>
                     <p className="text-sm opacity-70 hover:opacity-100 hover:dark:opacity-100">
-                      <Link href={"/dashboard/settings"} passHref>
-                        {t("navbar.settings")}
+                      <Link href="/post/privacy-policy" passHref>
+                        {t("privacy_policy.title")}
                       </Link>
                     </p>
                   </li>
+                  <li>
+                    <p className="text-sm opacity-70 hover:opacity-100 hover:dark:opacity-100">
+                      <Link href="/about" passHref>
+                        {t("navbar.about")}
+                      </Link>
+                    </p>
+                  </li>
+                  <li>
+                    <p className="text-sm opacity-70 hover:opacity-100 hover:dark:opacity-100">
+                      <Link href="/faq" passHref>
+                        {t("faq.nav")}
+                      </Link>
+                    </p>
+                  </li>{" "}
                 </ul>
-              </div>
+              </ul>
             </div>
-            <div className="md:grid md:gap-16 md:grid-cols-2">
-              <div>
-                <h3 className="font-semibold opacity-100 hover:opacity-80">
-                  {t("footer.stay_updated")}
-                </h3>
-                <ul className="mt-4 space-y-2" role="list">
-                  <li className="text-sm opacity-70 hover:opacity-100 hover:dark:opacity-100">
-                    <Link
-                      href="/"
-                      className="text-sm hover:text-opacity-60 hover:dark:opacity-50"
-                    >
-                      {t("footer.license")}
+            <div>
+              <Link
+                href={"/dashboard"}
+                className="font-semibold opacity-100 hover:opacity-80"
+              >
+                {t("navbar.dashboard")}
+              </Link>
+              <ul className="mt-4 space-y-2" role="list">
+                <li>
+                  <p className="text-sm opacity-70 hover:opacity-100 hover:dark:opacity-100">
+                    <Link href={"/dashboard/dictionaries"} passHref>
+                      {t("navbar.dictionaries")}
                     </Link>
-                  </li>
-                  <li className="text-sm opacity-70 hover:opacity-100 hover:dark:opacity-100">
-                    <Link
-                      href="/"
-                      className="text-sm hover:text-opacity-60 hover:dark:opacity-50"
-                    >
-                      {t("footer.documentation")}
+                  </p>
+                </li>
+              </ul>
+              <ul className="mt-4 space-y-2" role="list">
+                <li>
+                  <p className="text-sm opacity-70 hover:opacity-100 hover:dark:opacity-100">
+                    <Link href={"/dashboard/settings"} passHref>
+                      {t("navbar.settings")}
                     </Link>
-                  </li>
-                </ul>
-              </div>
-              <div>
-                <h3 className="font-semibold opacity-100 hover:opacity-80">
-                  {t("footer.socials")}
-                </h3>
-                <ul className="mt-4 space-y-2" role="list">
-                  <li className="text-sm opacity-70 hover:opacity-100 hover:dark:opacity-100">
-                    <Link
-                      href="https://twitter.com/wordigoapp"
-                      className="text-sm flex items-center gap-x-2 hover:text-opacity-60 hover:dark:opacity-50"
-                      target="_blank"
-                    >
-                      <TwitterIcon size={16} />
-                      Twitter
-                    </Link>
-                  </li>
-                  <li className="text-sm opacity-70 hover:opacity-100 hover:dark:opacity-100">
-                    <Link
-                      href="https://www.linkedin.com/company/wordigo"
-                      className="text-sm flex items-center gap-x-2 hover:text-opacity-60 hover:dark:opacity-50"
-                      target="_blank"
-                    >
-                      <LinkedinIcon size={16} />
-                      Linkedin
-                    </Link>
-                  </li>
-                  <li className="text-sm opacity-70 hover:opacity-100 hover:dark:opacity-100">
-                    <Link
-                      href="https://github.com/wordigo"
-                      className="text-sm flex items-center gap-x-2 hover:text-opacity-60 hover:dark:opacity-50"
-                      target="_blank"
-                    >
-                      <GithubIcon size={16} />
-                      Github
-                    </Link>
-                  </li>
-                </ul>
-              </div>
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="md:grid md:gap-16 md:grid-cols-2">
+            <div>
+              <h3 className="font-semibold opacity-100 hover:opacity-80">
+                {t("footer.stay_updated")}
+              </h3>
+              <ul className="mt-4 space-y-2" role="list">
+                <li className="text-sm opacity-70 hover:opacity-100 hover:dark:opacity-100">
+                  <Link
+                    href="/"
+                    className="text-sm hover:text-opacity-60 hover:dark:opacity-50"
+                  >
+                    {t("footer.license")}
+                  </Link>
+                </li>
+                <li className="text-sm opacity-70 hover:opacity-100 hover:dark:opacity-100">
+                  <Link
+                    href="/"
+                    className="text-sm hover:text-opacity-60 hover:dark:opacity-50"
+                  >
+                    {t("footer.documentation")}
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <h3 className="font-semibold opacity-100 hover:opacity-80">
+                {t("footer.socials")}
+              </h3>
+              <ul className="mt-4 space-y-2" role="list">
+                <li className="text-sm opacity-70 hover:opacity-100 hover:dark:opacity-100">
+                  <Link
+                    href="https://twitter.com/wordigoapp"
+                    className="text-sm flex items-center gap-x-2 hover:text-opacity-60 hover:dark:opacity-50"
+                    target="_blank"
+                  >
+                    <TwitterIcon size={16} />
+                    Twitter
+                  </Link>
+                </li>
+                <li className="text-sm opacity-70 hover:opacity-100 hover:dark:opacity-100">
+                  <Link
+                    href="https://www.linkedin.com/company/wordigo"
+                    className="text-sm flex items-center gap-x-2 hover:text-opacity-60 hover:dark:opacity-50"
+                    target="_blank"
+                  >
+                    <LinkedinIcon size={16} />
+                    Linkedin
+                  </Link>
+                </li>
+                <li className="text-sm opacity-70 hover:opacity-100 hover:dark:opacity-100">
+                  <Link
+                    href="https://github.com/wordigo"
+                    className="text-sm flex items-center gap-x-2 hover:text-opacity-60 hover:dark:opacity-50"
+                    target="_blank"
+                  >
+                    <GithubIcon size={16} />
+                    Github
+                  </Link>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
-        <div className="flex gap-y-2 flex-col">
-          <p className="text-left text-sm leading-loose md:text-left dark:text-white/50 text-black/50 mt-1">
-            {t("footer.copyright")}
-            <Link
-              href={"/"}
-              target="_blank"
-              rel="noreferrer"
-              className="font-medium underline underline-offset-4 ml-1"
-            >
-              Wordigo
-            </Link>
-            .
-          </p>
-        </div>
-      </div>
+      </section>
+      <section className="flex gap-y-2 flex-col">
+        <p className="text-left text-sm leading-loose md:text-left dark:text-white/50 text-black/50 mt-1">
+          {t("footer.copyright")}
+          <Link
+            href={"/"}
+            target="_blank"
+            rel="noreferrer"
+            className="font-medium underline underline-offset-4 ml-1"
+          >
+            Wordigo
+          </Link>
+          .
+        </p>
+      </section>
     </footer>
   );
 }
