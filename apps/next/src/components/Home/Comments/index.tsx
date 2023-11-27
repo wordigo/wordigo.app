7b005fb50ca508273648cebd2b7ca37e39ab5b0f
Emre Erden <emreerden@pm.me>
2023-11-27 17:49:45 +0300

    edit Comment structure

--- apps/next/src/components/Home/Comments/index.tsx
+++ apps/next/src/components/Home/Comments/index.tsx
@@ -6,7 +6,7 @@ const CommentSection = () => {
   const { t } = useTranslation();
 
   return (
-    <section className="px-20 py-24 pb-12 w-full flex flex-col items-center max-lg:px-4">
+    <section className="flex flex-col items-center">
       <h2 className="text-4xl font-semibold">{t("testimonials.heading")}</h2>
       <p className="text-xl text-muted-foreground mt-5">{t("testimonials.description")}</p>
       <div className="w-full mt-16">
