beb87e8b7b9ee603e826990519e2e45248255ae6
Emre Erden <emreerden@pm.me>
2023-12-01 14:44:59 +0300

    style: edit MainLayout padding

--- apps/next/src/components/Layout/MainLayout/index.tsx
+++ apps/next/src/components/Layout/MainLayout/index.tsx
@@ -11,7 +11,7 @@ const MainLayout: React.FC<PropsWithChildren<{ showBanner?: boolean }>> = ({
     <>
       {showBanner && <Banner />}
       <HomeHeader />
-      <main className="container flex flex-col gap-y-16 sm:gap-y-20 md:gap-y-24 lg:gap-y-28 pt-16 sm:pt-20 md:pt-24 pb-[72px] sm:pb-24 md:pb-[104px] lg:pb-28">
+      <main className="container flex flex-col gap-y-16 sm:gap-y-20 md:gap-y-24 lg:gap-y-28 xl:gap-y-32 pt-6 sm:pt-10 md:pt-14 lg:pt-[72px] xl:pt-[88px] pb-[72px] sm:pb-24 md:pb-[104px] lg:pb-28">
         {children}
       </main>
       <Footer />
