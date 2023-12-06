5155794c97e93bc6a89d1edc144088d3ca81930c
osmandlsmn <dlsmnosman@gmail.com>
2023-12-06 22:11:01 +0300

    feat: dashboard word interaction api integration

--- apps/next/src/components/Dashboard/Overview/Statistics/Overview.tsx
+++ apps/next/src/components/Dashboard/Overview/Statistics/Overview.tsx
@@ -1,36 +1,41 @@
-import { Card, CardContent } from "@wordigo/ui";
+import { useGetOverviewStatisticsQuery } from "@/store/profile/api";
+import { Card, CardContent, Skeleton } from "@wordigo/ui";
 import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis } from "recharts";
 
-const data = Array.from({ length: 30 }, (_, i) => {
-  return {
-    name: i + 1,
-    total: Math.random() > 0.2 ? Math.floor(Math.random() * 500) + 50 : 0,
-  };
-});
-
 export function DashboardOverview() {
+  const { data: response, isLoading } = useGetOverviewStatisticsQuery("");
+
+  const computedData = response?.data?.map((item) => ({
+    name: item.day,
+    total: item.words,
+  }));
+
   return (
     <Card className="">
       <CardContent className="!p-0">
-        <ResponsiveContainer width="100%" height={350}>
-          <BarChart data={data}>
-            <XAxis
-              dataKey="name"
-              stroke="#888888"
-              fontSize={12}
-              tickLine={false}
-              axisLine={false}
-            />
-            <YAxis
-              stroke="#888888"
-              fontSize={12}
-              tickLine={false}
-              axisLine={false}
-              tickFormatter={(value) => `${value}`}
-            />
-            <Bar dataKey="total" fill="#4a90e2" radius={[4, 4, 0, 0]} />
-          </BarChart>
-        </ResponsiveContainer>
+        {isLoading ? (
+          <Skeleton className="h-80" />
+        ) : (
+          <ResponsiveContainer width="100%" height={350}>
+            <BarChart data={computedData}>
+              <XAxis
+                dataKey="name"
+                stroke="#888888"
+                fontSize={12}
+                tickLine={false}
+                axisLine={false}
+              />
+              <YAxis
+                stroke="#888888"
+                fontSize={12}
+                tickLine={false}
+                axisLine={false}
+                tickFormatter={(value) => `${value}`}
+              />
+              <Bar dataKey="total" fill="#4a90e2" radius={[4, 4, 0, 0]} />
+            </BarChart>
+          </ResponsiveContainer>
+        )}
       </CardContent>
     </Card>
   );
