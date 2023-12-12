b732b1849423e49a58a0f217decaaa0e3550ff8f
Emre Erden <emreerden@pm.me>
2023-12-12 17:24:21 +0300

    style: edit Team Members Section

--- apps/next/src/components/About/Team/team.tsx
+++ apps/next/src/components/About/Team/team.tsx
@@ -8,14 +8,14 @@ const TeamSection = () => {
   const { t } = useTranslation();
 
   return (
-    <section className="flex flex-col gap-8 sm:gap-12 md:gap-16 lg:gap-20">
+    <section className="flex flex-col gap-8 sm:gap-10 md:gap-12 lg:gap-14 xl:gap-16">
       <div className="text-center max-w-sm sm:max-w-xl md:max-w-2xl lg:max-w-4xl xl:max-w-5xl mx-auto">
         <h2 className="font-semibold text-2xl sm:text-3xl md:text-4xl">
           {t("about.team.heading")}
         </h2>
       </div>
 
-      <div className="w-full grid grid-cols-3 grid-rows-2 gap-x-4 gap-y-16 max-lg:grid-cols-3 max-md:grid-cols-2">
+      <div className="w-full md:w-3/4 mx-auto grid grid-cols-4 grid-rows-2 gap-x-4 gap-y-8 sm:gap-y-10 md:gap-y-12 lg:gap-y-14 xl:gap-y-16 max-lg:grid-cols-3 max-md:grid-cols-2">
         {commentConstants.map((item) => (
           <TeamSection.Item key={item.title} {...item} />
         ))}
@@ -26,17 +26,18 @@ const TeamSection = () => {
 
 TeamSection.Item = ({ github, linkedin, name, src, title, twitter }: ITeam) => {
   return (
-    <div className="flex flex-col items-center justify-between p-4">
-      <div className="flex flex-col items-center">
-        <div className="relative w-36 h-36">
+    <div className="flex flex-col items-center justify-between gap-y-4">
+      <div className="flex flex-col items-center gap-y-4 text-center">
+        <div className="relative w-24 h-24">
           <Image className="rounded-full" alt="" src={src} fill></Image>
         </div>
-        <h3 className="text-lg font-semibold mt-5 text-center">{name}</h3>
-        <p className="mt-2 text-base text-muted-foreground text-center">
-          {title}
-        </p>
+
+        <h3 className="text-lg font-semibold">{name}</h3>
+
+        <p className="text-muted-foreground">{title}</p>
       </div>
-      <div className="mt-4 grid grid-flow-col gap-x-4 text-muted-foreground">
+
+      <div className="grid grid-flow-col gap-x-4 text-muted-foreground">
         {twitter && (
           <Link
             href={twitter}
