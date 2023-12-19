dd2519d35cb65bbfd3d165bffab4ff579711b394
yusufgns <gunesyusuf838@gmail.com>
2023-12-19 22:03:43 +0300

    fix: blog page style and content

--- apps/next/src/pages/blogs/[slug]/index.tsx
+++ apps/next/src/pages/blogs/[slug]/index.tsx
@@ -8,6 +8,7 @@ import { type GetStaticPaths, type InferGetStaticPropsType } from "next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { MDXRemote } from "next-mdx-remote";
 import Image from "next/image";
+import Link from "next/link";
 import path from "path";
 import { useState } from "react";
 import { FaBookmark, FaRegBookmark } from "react-icons/fa";
@@ -19,11 +20,11 @@ export default function BlogDetailPage({
   source,
   info,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
-  const [isBlogSaved, setIsBlogSaved] = useState(true);
-
   const host =
     typeof window !== "undefined" ? window.location.origin : undefined;
   const url = `${host}/blogs/${info?.slug}`;
+
+  console.log(source);
 
   const copyToClipboard = () => {
     if (typeof navigator !== "undefined" && navigator.clipboard) {
@@ -35,6 +36,19 @@ export default function BlogDetailPage({
     }
   };
 
+  const components = {
+    h1: (props) => (
+      <p {...props} className="font-bold">
+        {""}
+      </p>
+    ),
+    p: (props) => (
+      <p {...props} className="blogStle">
+        {props.children}
+      </p>
+    ),
+  };
+
   return (
     <MainLayout>
       <div className="flex justify-center p-0 pt-0 mt-2 px-0">
@@ -42,19 +56,12 @@ export default function BlogDetailPage({
           <div className="flex justify-between w-full mt-2 md:mt-5">
             <div className=" w-full flex flex-col gap-2 md:gap-6">
               <div className="flex w-full gap-2 md:gap-0 items-center justify-between">
-                <div className="text-xl md:text-3xl">{info?.title}</div>{" "}
+                <h1 className="text-xl md:text-3xl font-bold">{info?.title}</h1>{" "}
                 <div className=" py-1 w-fit flex items-center px-2 rounded-[0.625rem] text-xs font-medium border justify-center whitespace-nowrap">
                   {info?.date} · 12 min read
                 </div>{" "}
               </div>
 
-              <div className="bg-gray-600 w-full h-52 rounded-lg overflow-hidden relative">
-                <Image
-                  src={`/images/blogs/${info?.thumbnail}`}
-                  fill
-                  alt={info?.title}
-                />
-              </div>
               <div className="flex flex-col md:flex-row gap-4 md:gap-0 justify-between items-center">
                 <div className="flex items-center gap-3 pt-2 w-full">
                   <Image
@@ -73,39 +80,37 @@ export default function BlogDetailPage({
                 </div>
                 <div className=" flex  items-center gap-7 md:w-fit w-full md:justify-normal justify-between">
                   <div className="flex items-center gap-2">
-                    <div className="dark:text-white text-black transition-all py-1 w-fit flex items-center px-2 rounded-[0.625rem]  font-medium border text-sm gap-2 justify-center cursor-pointer dark:bg-DarkBackground whitespace-nowrap hover:bg-slate-200 ">
-                      <XIcon className="w-4 dark:fill-white fill-black" />
-                      Tweet
-                    </div>
                     <Button
                       variant="outline"
-                      className="text-xs px-4 w-20"
+                      className="transition-all py-1 w-fit flex items-center px-2 font-medium border text-sm gap-2 justify-center cursor-pointerwhitespace-nowrap dark:bg-transparent"
+                    >
+                      <XIcon className="w-4 dark:fill-white fill-black" />
+                      Tweet
+                    </Button>
+                    <Button
+                      variant="outline"
+                      className="text-xs min-w-fit dark:bg-transparent"
                       onClick={copyToClipboard}
                     >
                       <MdContentCopy className="w-4 dark:fill-white fill-black" />
-                      Copy Link
+                      <p className="w-20"> Copy Link</p>
                     </Button>
                     {/* dark:text-white text-black transition-all py-1 w-fit flex items-center px-2 rounded-[0.625rem]  dark:bg-DarkBackground font-medium border text-sm gap-2 justify-center cursor-pointer whitespace-nowrap hover:bg-slate-200 */}
                   </div>
-                  <div>
-                    {isBlogSaved ? (
-                      <FaBookmark
-                        onClick={() => setIsBlogSaved(!isBlogSaved)}
-                        className="w-6 fill-black dark:fill-white cursor-pointer"
-                      />
-                    ) : (
-                      <FaRegBookmark
-                        onClick={() => setIsBlogSaved(!isBlogSaved)}
-                        className="w-6 fill-black dark:fill-white cursor-pointer"
-                      />
-                    )}
-                  </div>
                 </div>
+              </div>
+
+              <div className="bg-gray-600 w-full h-52 rounded-lg overflow-hidden relative">
+                <Image
+                  src={`/images/blogs/${info?.thumbnail}`}
+                  fill
+                  alt={info?.title}
+                />
               </div>
             </div>
           </div>
-          <article className="prose dark:prose-dark min-w-full border-t-[1px] border-gray-300 pt-4">
-            <MDXRemote {...source} />
+          <article className="prose dark:prose-dark min-w-full pt-4 break-words dark:text-white">
+            <MDXRemote {...source} components={{ ...components }} />
           </article>
         </div>
       </div>
